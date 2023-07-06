94f23bf7cc933a9985c4e76a16d6aa43fb4142b9
Eliott-B <eliottb.info@gmail.com>
2023-07-06 15:42:33 +0200

    Script

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Eliott-B'
+description 'Add extra on undercover car if F7 is pressed.'
+version '1.0.0'
+
+resource_type 'gametype'
+
+client_script 'src/extra.lua'
---
+++ src/extra.lua
@@ -0,0 +1,47 @@
+-- client side
+RegisterKeyMapping('beacon', ' My Keypress Key', 'keyboard', 'f7')
+
+RegisterCommand('beacon', function()
+
+    local playerPed = PlayerPedId()
+
+    if (DoesEntityExist(playerPed) and not IsEntityDead(playerPed)) then
+
+        if (IsPedSittingInAnyVehicle(playerPed)) then 
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+
+            if (GetPedInVehicleSeat( vehicle, -1) == playerPed) then 
+                local model = GetEntityModel(vehicle)
+                local name = GetDisplayNameFromVehicleModel(model)
+
+                if (name == 'samu') then
+
+                    if(IsVehicleExtraTurnedOn(vehicle,1)) then
+                        SetVehicleExtra(vehicle, 1, true)
+                        SetVehicleExtra(vehicle, 4, true)
+                        Notify('~y~Extra removed!')
+                    else
+                        SetVehicleExtra(vehicle, 1, false)
+                        SetVehicleExtra(vehicle, 4, false)
+                        Notify('~y~Extra added!')
+                    end
+
+                else
+                    Notify('~r~~h~ERROR :~h~ you are not in a Peugeot 508 BANALISÉ !')
+                end
+
+            else 
+                Notify('~r~~h~ERROR :~h~ you are not the driver !')
+            end 
+
+        else
+            Notify('~r~~h~ERROR :~h~ you are not in a vehicule !')
+        end 
+    end
+end)
+
+function Notify( text )
+    SetNotificationTextEntry( "STRING" )
+    AddTextComponentString( text )
+    DrawNotification( false, false )
+end
