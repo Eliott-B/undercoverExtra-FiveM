8fb3d81ffa7977ebb293e40126b966818b3fb521
Eliott-B <eliottb.info@gmail.com>
2023-07-07 02:55:38 +0200

    🪄 | Full optimisation
    Thanks JellyPh1sh
    
    Co-Authored-By: JellyPhish <64471702+jellyph1sh@users.noreply.github.com>

---
+++ eb_undercoverExtra/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.VehiclesModels = {
+    ["cliobana"] = {
+        ["group1"] = {8, 9}
+    },
+    ["508bana"] = {
+        ["group1"] = {1, 4}
+    },
+    ["peug_207"] = {
+        ["group1"] = {1, 3}
+    },
+    ["expertbana"] = {
+        ["group1"] = {1, 3},
+        ["group2"] = {4}
+    }
+}
